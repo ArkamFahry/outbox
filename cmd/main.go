ec44e56b39463864e42a79179fbee866ee17cc48
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-02-23 22:00:40 +0530

    refactor: moved main to main/cmd

---
+++ cmd/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"github.com/ArkamFahry/outbox/internal"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
+	"go.uber.org/zap"
+)
+
+func main() {
+	config := internal.NewConfig()
+
+	logger := internal.NewLogger(config)
+
+	natsClient, err := nats.Connect(config.NatsUrl)
+	if err != nil {
+		logger.Fatal("error connecting to nats", zap.Error(err))
+	}
+
+	jetStreamsClient, err := jetstream.New(natsClient)
+	if err != nil {
+		logger.Fatal("error connecting to jetstream", zap.Error(err))
+	}
+
+	pgxPoolConfig, err := pgxpool.ParseConfig(config.PostgresUrl)
+	if err != nil {
+		logger.Fatal("error parsing postgres url",
+			zap.Error(err),
+		)
+	}
+
+	pgxPool, err := pgxpool.NewWithConfig(context.Background(), pgxPoolConfig)
+	if err != nil {
+		logger.Fatal("error connecting to postgres",
+			zap.Error(err),
+		)
+	}
+
+	database := internal.NewDatabase(pgxPool, config)
+
+	eventStore := internal.NewPublisher(jetStreamsClient, config)
+
+	publisher := internal.NewEventPublisherWorker(database, eventStore, logger)
+
+	for {
+		err = publisher.Work(context.Background())
+		if err != nil {
+			logger.Error("error publishing event",
+				zap.Error(err),
+			)
+		}
+	}
+}
